7230479edf7877931ee3b8013815ed7d7bf348e9
wzw <1258999146@qq.com>
2020-05-02 12:12:01 +0800

    第一次测试GIT

---
+++ src/test/Demo.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Demo {
+    public void getSay(String words){
+        words="Good ";
+        System.out.println(words);
+    }
+    public static void main(String[] args) {
+        int a=5;
+        int b=a;
+        System.out.println("2313123131");
+    }
+}
