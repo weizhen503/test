7baf13754814418d55b2a11438d4c240e4acbf59
wzw <1258999146@qq.com>
2020-10-14 16:46:04 +0800

    1111

--- src/leetcode/大学算法训练营/line/todo/MergeSortedArr88.java
+++ src/leetcode/大学算法训练营/line/todo/MergeSortedArr88.java
@@ -6,7 +6,19 @@ package leetcode.大学算法训练营.line.todo;
  */
 public class MergeSortedArr88 {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
+        int i = m - 1;
+        int j = n - 1;
+        for(int k = m + n - 1 ; k >= 0 ; k --){
+            if(i < 0){
+                nums1[k] = nums2[j --];
+            }else if(j < 0){
+                nums1[k] = nums1[i --];
+            }else if(nums1[i] < nums2[j]){
+                nums1[k] = nums2[j--];
+            }else {
+                nums1[k] = nums1[i--];
+            }
+        }
     }
 
     public static void main(String[] args) {
--- src/leetcode/大学算法训练营/line/todo/RotateArr189.java
+++ src/leetcode/大学算法训练营/line/todo/RotateArr189.java
@@ -4,12 +4,42 @@ package leetcode.大学算法训练营.line.todo;
  * @date:2020/10/13 7:41
  * @version:1.0
  */
-public class RotateArr189 {
-    public void rotate(int[] nums, int k) {
+public class RotateArr189{
+    public void rotate(int[] nums, int k){
+//暴力解法
+// for(int j = nums.length - 1 ; j >= nums.length - k ; j --){
+//     int temp = nums[nums.length - 1];
+//     for(int i = nums.length - 2 ;  i >= 0 ; i --){
+//         nums[i + 1] = nums[i];
+//     }
+//     nums[0] = temp;
+// }
+
+//创建新数组法
+// int newarr[] = new int[nums.length];
+//  for(int i = 0 ; i < nums.length ; i ++){
+//  newarr[(i+k)%nums.length] = nums[i];
+//  }
+// for(int i = 0 ; i < nums.length ; i ++){
+//     nums[i] = newarr[i];
+// }
+
+//反转法
+        k %= nums.length;
+        reverse(nums,0,nums.length - 1);
+        reverse(nums,0,k - 1);
+        reverse(nums, k,nums.length-1);
 
     }
 
-    public static void main(String[] args) {
-        
+    public void reverse(int[]nums,int start,int end){
+        while(start < end){
+            int temp_end = nums[end];
+            nums[end] = nums[start];
+            nums[start] = temp_end;
+            start ++;
+            end --;
+        }
     }
 }
+
