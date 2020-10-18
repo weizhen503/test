99e0d099e6cf4f9ffae12071351e9c9bf528f58b
wzw <1258999146@qq.com>
2020-10-18 10:54:35 +0800

    111

---
+++ src/leetcode/大学算法训练营/line/todo/ClimbStairs70.java
@@ -0,0 +1,18 @@
+package leetcode.大学算法训练营.line.todo;
+
+public class ClimbStairs70 {
+    public int climbStairs(int n) {
+        int first = 1;
+        int second = 2;
+        if (n<=2){
+            return n;
+        }
+        int sum = 0;
+        for(int i = 3 ; i <= n ; i ++){
+            sum = first + second;
+            first = second;
+            second = sum;
+        }
+        return sum;
+    }
+}
