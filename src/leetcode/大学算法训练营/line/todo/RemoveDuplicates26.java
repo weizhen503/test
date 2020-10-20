2beba9d63e17ca4498e71cd2d11aa42b12fa62ff
wzx <weizhenxiong@xiao100.com>
2020-10-20 15:25:23 +0800

    算法资料

--- src/leetcode/大学算法训练营/line/todo/RemoveDuplicates26.java
+++ src/leetcode/大学算法训练营/line/todo/RemoveDuplicates26.java
@@ -1,17 +1,14 @@
 package leetcode.大学算法训练营.line.todo;
 
-import leetcode.大学算法训练营.util.ListNode;
-
 /**
  * @author: wzx
  * @date:2020/10/19 7:48
  * @version:1.0
  */
 public class RemoveDuplicates26 {
-    public ListNode reverseKGroup(ListNode head, int k) {
-        return null;
+    public int removeDuplicates(int[] nums) {
+        return 0;
     }
-
     public static void main(String[] args) {
 
     }
--- src/leetcode/大学算法训练营/line/todo/ReverseKGroup25.java
+++ src/leetcode/大学算法训练营/line/todo/ReverseKGroup25.java
@@ -4,6 +4,7 @@ package leetcode.大学算法训练营.line.todo;
 import leetcode.大学算法训练营.util.ListNode;
 
 /**
+ * 资料： 1. materials\fucking-algorithm-master\高频面试系列\k个一组反转链表
  * @author: wzx
  * @date:2020/10/19 7:49
  * @version:1.0
