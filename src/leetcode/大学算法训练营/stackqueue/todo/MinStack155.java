e92abab921fdd13ef1a3899ef4e326c4dfab0723
wzx <weizhenxiong@xiao100.com>
2020-11-02 13:50:56 +0800

    题目安排

---
+++ src/leetcode/大学算法训练营/stackqueue/todo/MinStack155.java
@@ -0,0 +1,30 @@
+package leetcode.大学算法训练营.stackqueue.todo;
+
+/**
+ * @author: wzx
+ * @date:2020/11/2 13:47
+ * @version:1.0
+ */
+public class MinStack155 {
+
+    /** initialize your data structure here. */
+    public MinStack155() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+        return 0;
+    }
+
+    public int getMin() {
+        return 0;
+    }
+}
---
+++ src/leetcode/大学算法训练营/stackqueue/todo/MyCircularDeque641.java
@@ -0,0 +1,53 @@
+package leetcode.大学算法训练营.stackqueue.todo;
+
+/**
+ * @author: wzx
+ * @date:2020/11/2 13:48
+ * @version:1.0
+ */
+public class MyCircularDeque641 {
+    /** Initialize your data structure here. Set the size of the deque to be k. */
+    public MyCircularDeque641(int k) {
+
+    }
+
+    /** Adds an item at the front of Deque. Return true if the operation is successful. */
+    public boolean insertFront(int value) {
+        return false;
+    }
+
+    /** Adds an item at the rear of Deque. Return true if the operation is successful. */
+    public boolean insertLast(int value) {
+        return false;
+    }
+
+    /** Deletes an item from the front of Deque. Return true if the operation is successful. */
+    public boolean deleteFront() {
+        return false;
+    }
+
+    /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
+    public boolean deleteLast() {
+        return false;
+    }
+
+    /** Get the front item from the deque. */
+    public int getFront() {
+        return 0;
+    }
+
+    /** Get the last item from the deque. */
+    public int getRear() {
+            return 0;
+    }
+
+    /** Checks whether the circular deque is empty or not. */
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /** Checks whether the circular deque is full or not. */
+    public boolean isFull() {
+        return false;
+    }
+}
